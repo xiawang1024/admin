3017ba6f26349328697744e27d1305dcc0d90b49
wangxia/work <xiawang1024@icloud.com>
2019-01-08 09:07:06 +0800

    init

---
+++ src/components/ViewType/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-form
+    ref="form"
+    label-width="80px"
+    class="form-wrap"
+  >
+    <el-form-item
+      label="视图："
+      class="form-item"
+    >
+      <el-select
+        v-model="selectedType"
+        placeholder="请选择"
+      >
+        <el-option
+          v-for="item in viewType"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      viewType: [
+        {
+          value: '选项1',
+          label: '文档'
+        },
+        {
+          value: '选项2',
+          label: '模板'
+        },
+        {
+          value: '选项3',
+          label: '图片组'
+        },
+        {
+          value: '选项4',
+          label: '自定义列表'
+        },
+        {
+          value: '选项5',
+          label: 'HTML片段'
+        },
+        {
+          value: '选项6',
+          label: '废件箱'
+        }
+      ],
+      selectedType: '选项1'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form-wrap {
+  height: 50px;
+  .form-item {
+    margin-bottom: 0;
+    display: inline-block;
+  }
+}
+</style>
+
---
+++ src/views/webSite/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="components-container">
+
+    <split-pane
+      :min-percent="10"
+      :max-percent="30"
+      :default-percent="15"
+      split="vertical"
+      @resize="resize"
+    >
+      <template slot="paneL">
+        <div class="left-container">
+          <el-scrollbar
+            wrap-class="scrollbar-wrapper"
+            style="height:100%;"
+          >
+            <tree/>
+          </el-scrollbar>
+        </div>
+      </template>
+      <template slot="paneR">
+        <div class="right-container">
+          <router-view />
+        </div>
+      </template>
+    </split-pane>
+
+  </div>
+</template>
+
+<script>
+import splitPane from 'vue-splitpane'
+import Tree from '@/components/Tree'
+
+export default {
+  name: 'WebSiteWrap',
+  components: { splitPane, Tree },
+  methods: {
+    resize() {
+      console.log('resize')
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.components-container {
+  position: relative;
+  height: calc(100vh - 124px);
+  margin: 20px 10px;
+}
+
+.left-container {
+  /* background-color: #f38181; */
+  height: 100%;
+  /* overflow: hidden; */
+}
+
+.right-container {
+  // background-color: #fce38a;
+  height: 100%;
+}
+</style>
