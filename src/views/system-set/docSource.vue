5c24fcfcb614a99f1e596344c56de3595aef41db
Xavier <38767672@qq.com>
2019-01-21 18:16:51 +0800

    写了前两个

--- src/views/system-set/docSource.vue
+++ src/views/system-set/docSource.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="docSource-container">
-    <p>DocSource</p>
+    <div class="tool-bar">
+      <el-button type="primary" @click="addDocSourceDialogVisible = true">新增</el-button>
+    </div>
+    <el-table :data="siteList" style="width: 100%">
+      <el-table-column prop="name" label="来源名称"></el-table-column>
+      <el-table-column prop="location" label="来源路径"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="prime" @click="beforeAlter(scope.$index, scope.row)">修改</el-button>
+          <el-button type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog title="添加文稿来源" :visible.sync="addDocSourceDialogVisible">
+      <el-form :model="docSourceForm">
+        <el-form-item label="来源名称">
+          <el-input v-model="docSourceForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="来源路径">
+          <el-input v-model="docSourceForm.location"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDocSourceDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAdd()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog title="修改文稿来源" :visible.sync="alterDocSourceDialogVisible">
+      <el-form :model="docSourceForm">
+        <el-form-item label="来源名称">
+          <el-input v-model="docSourceForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="来源路径">
+          <el-input v-model="docSourceForm.location"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="alterDocSourceDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAlter()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -8,14 +50,64 @@
 export default {
   name: "DocSource",
   data() {
-    return {};
+    return {
+      siteList: [
+        {
+          id: "1",
+          name: "网站名称",
+          location: "site.name"
+        },
+        {
+          id: "2",
+          name: "生产服务器",
+          location: "factory.domain"
+        },
+        {
+          id: "3",
+          name: "发布根目录",
+          location: "publish.root"
+        },
+        {
+          id: "4",
+          name: "发布域名",
+          location: "site.domain"
+        }
+      ],
+      addDocSourceDialogVisible: false,
+      alterDocSourceDialogVisible: false,
+      docSourceForm: {
+        id: "",
+        name: "",
+        location: ""
+      }
+    };
   },
-  methods: {}
+  methods: {
+    handleAdd() {
+      console.log("新增");
+    },
+    beforeAlter(index,row) {
+      this.docSourceForm.id = row.id;
+      this.docSourceForm.name = row.name;
+      this.docSourceForm.location = row.location;
+      this.alterDocSourceDialogVisible = true;
+    },
+    handleAlter() {
+      console.log("修改");
+    },
+    handleDelete(index, row) {
+      console.log("删除" + row.name);
+    }
+  }
 };
 </script>
 
 <style scoped>
-.tab-container {
+.docSource-container {
   margin: 30px;
+}
+
+.tool-bar {
+  text-align: right;
 }
 </style>
--- src/views/system-set/platformSet.vue
+++ src/views/system-set/platformSet.vue
@@ -1,24 +1,118 @@
 <template>
-  <div class="platform-set-container">
-    hah 
+  <div class="platform-container">
+    <div class="tool-bar">
+      <el-button type="primary" @click="addDocSourceDialogVisible = true">新增</el-button>
+    </div>
+    <el-table :data="siteList" style="width: 100%">
+      <el-table-column prop="plat" label="来源名称"></el-table-column>
+      <el-table-column prop="desc" label="来源路径"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="prime" @click="beforeAlter(scope.$index, scope.row)">修改</el-button>
+          <el-button type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog title="添加文稿来源" :visible.sync="addDocSourceDialogVisible">
+      <el-form :model="docSourceForm">
+        <el-form-item label="来源名称">
+          <el-input v-model="docSourceForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="来源路径">
+          <el-input v-model="docSourceForm.location"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDocSourceDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAdd()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog title="修改文稿来源" :visible.sync="alterDocSourceDialogVisible">
+      <el-form :model="docSourceForm">
+        <el-form-item label="来源名称">
+          <el-input v-model="docSourceForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="来源路径">
+          <el-input v-model="docSourceForm.location"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="alterDocSourceDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAlter()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'PlatformSet',
+  name: "DocSource",
   data() {
     return {
-    }
+      siteList: [
+        {
+          id: "1",
+          plat: "网站名称",
+          desc: "site.name",
+          fileName: ""
+        },
+        {
+          id: "2",
+          plat: "生产服务器",
+          desc: "factory.domain",
+          fileName: ""
+        },
+        {
+          id: "3",
+          plat: "发布根目录",
+          desc: "publish.root",
+          fileName: ""
+        },
+        {
+          id: "4",
+          plat: "发布域名",
+          desc: "site.domain",
+          fileName: ""
+        }
+      ],
+      addDocSourceDialogVisible: false,
+      alterDocSourceDialogVisible: false,
+      docSourceForm: {
+        id: "",
+        plat: "",
+        desc: "",
+        fileName: ""
+      }
+    };
   },
   methods: {
+    handleAdd() {
+      console.log("新增");
+    },
+    beforeAlter(index, row) {
+      this.docSourceForm.id = row.id;
+      this.docSourceForm.plat = row.plat;
+      this.docSourceForm.desc = row.desc;
+      this.alterDocSourceDialogVisible = true;
+    },
+    handleAlter() {
+      console.log("修改");
+    },
+    handleDelete(index, row) {
+      console.log("删除" + row.plat);
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-  .tab-container{
-    margin: 30px;
-  }
+.platform-container {
+  margin: 30px;
+}
+
+.tool-bar {
+  text-align: right;
+}
 </style>
