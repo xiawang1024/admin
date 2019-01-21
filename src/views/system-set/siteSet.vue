e1c4bce5639306ef1119abc4cf02c563c665b9a3
Xavier <38767672@qq.com>
2019-01-21 16:06:12 +0800

    站点shezhi

--- src/views/system-set/siteSet.vue
+++ src/views/system-set/siteSet.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="site-set-container">
-    <p>SiteSet</p>
+    <el-table :data="siteList" style="width: 100%">
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="code" label="代码"></el-table-column>
+      <el-table-column label="值">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.value"></el-input>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-button type="primary" class="btn-save">保存</el-button>
   </div>
 </template>
 
@@ -8,14 +17,41 @@
 export default {
   name: "SiteSet",
   data() {
-    return {};
+    return {
+      siteList: [
+        {
+          name: "网站名称",
+          code: "site.name",
+          value: "河南广播网"
+        },
+        {
+          name: "生产服务器",
+          code: "factory.domain",
+          value: "http://admin.hndt.com"
+        },
+        {
+          name: "发布根目录",
+          code: "publish.root",
+          value: "/home/pub/"
+        },
+        {
+          name: "发布域名",
+          code: "site.domain",
+          value: "http://www.hndt.com"
+        }
+      ]
+    };
   },
   methods: {}
 };
 </script>
 
 <style scoped>
-.tab-container {
+.site-set-container {
   margin: 30px;
+}
+
+.btn-save {
+  margin-top: 20px;
 }
 </style>
