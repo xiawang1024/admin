ab25bc461a313b0d465ffcdee60d680dd907c0ef
Xavier <38767672@qq.com>
2019-01-22 15:20:36 +0800

    资源文件管理

--- src/views/system-set/sourceManage.vue
+++ src/views/system-set/sourceManage.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="source-manage-container">
-    <p>是的</p>
+    <div class="tool-bar clearfix">
+      <el-tag type="success" style="margin-left:20px;font-size: 16px">路径：{{location}}</el-tag>
+      <el-button
+        type="text"
+        style="margin-left:20px;font-size: 16px"
+        @click="refresh"
+        icon="el-icon-refresh"
+      >刷新</el-button>
+      <el-button type="text" style="margin-left:20px;font-size: 16px" @click="back">返回上级目录</el-button>
+      <el-popover placement="bottom-start" v-model="popoverVisible">
+        <div style="text-align: right; margin: 0">
+          <el-input :rows="2" v-model="folderName" style="margin-bottom:10px;" placeholder="请输入文件夹名称"></el-input>
+          <el-button type="primary" size="mini" @click="popoverVisible = false">确定</el-button>
+        </div>
+        <el-button slot="reference" type="primary pull-right" style="margin-left:20px" @click="popoverVisible = true">新建文件夹</el-button>
+      </el-popover>
+      <el-button type="primary pull-right" @click="upload">上传</el-button>
+    </div>
+    <el-table :data="sourceFileList" style="width: 100%">
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="size" label="大小"></el-table-column>
+      <el-table-column prop="type" label="类别"></el-table-column>
+      <el-table-column prop="updateTime" label="修改时间"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="prime" @click="sync(scope.$index, scope.row)" title="同步资源文件到发布目录">同步</el-button>
+          <el-button type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -8,14 +37,52 @@
 export default {
   name: "sourceManage",
   data() {
-    return {};
+    return {
+      location: "/GEN/WORK",
+      sourceFileList: [
+        {
+          id: 1,
+          name: "WORK",
+          size: "20KB",
+          type: "文件夹",
+          updateTime: "2018-01-02 12：24"
+        }
+      ],
+      popoverVisible:false,
+      folderName: ""
+    };
   },
-  methods: {}
+  methods: {
+    refresh() {},
+    upload() {},
+    createFolder() {},
+    back() {},
+    sync() {},
+    handleDelete() {}
+  }
 };
 </script>
 
 <style scoped>
-.tab-container {
+.el-button + .el-button {
+  margin-top: 5px;
+  margin-left: 0px;
+}
+
+.source-manage-container {
   margin: 30px;
+}
+
+.pull-right {
+  float: right;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
 }
 </style>
